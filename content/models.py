02f81238088bbe849d058ea8d8ea9e1e7d1e004f
KHUCHAN <cksdudo985@naver.com>
2022-11-30 14:46:26 +0900

    db 수정2

--- content/models.py
+++ content/models.py
@@ -1,8 +1,14 @@
 from django.db import models
-
+3
 # Create your models here.
 class Feed(models.Model):
-    location = models.TextField()
+    name = models.TextField()
     image = models.TextField()
-    click = models.TextField()
-    conv = models.TextField()
+    location = models.TextField()
+    convenience = models.TextField(default=0)
+    explain = models.TextField()
+
+class Reply(models.Model):
+    content = models.TextField()
+    feed = models.ForeignKey(Feed, on_delete=models.CASCADE)
+
--- content/views.py
+++ content/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import  render , redirect
+import json
+from django.http import JsonResponse
+from .models import Reply
 
 # Create your views here.
+def reply(request):
+    jsonObject = json.loads(request.body)
+    reply = Reply.objects.create(
+        feed_id=jsonObject.get('feedid'),
+        content=jsonObject.get('content'))
+    reply.save()
+    context = {
+        'name': reply.feed.name,
+        'content': reply.content,
+
+    }
+
+    return JsonResponse(context);
+
+def detail(request,feedid):
+    feed = get_object_or_404(Feed,pk=feedid)
+    reply = Reply.objects.filter(feed_id=feedid)
+    try:
+        session = request.session['feedid']
+        context = {
+            'feed':feed,
+            'reply':reply
+        }
+        return render(request, 'API MAP.html', context)
+    except KeyError:
+        return redirect('main')
