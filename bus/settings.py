777891f54d6b0013e47ae2f5cf3e9d9b2a316d6d
Subin Park <sbinp5818@khu.ac.kr>
2023-01-03 02:55:53 +0900

    웹사이트 배포-migrate 파일 커밋

--- bus/settings.py
+++ bus/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-q97s1ip5nquaxaydwr75s*0fw2z2_u0w6i2b@!nhy#(hjpl0_(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
